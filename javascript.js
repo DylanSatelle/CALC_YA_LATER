519bc0bc70cf1f820e85b4be3cfb3483d5bcb81d
Dylan Satelle <dylanjsatelle@gmail.com>
2024-06-16 08:48:38 +0100

    added add, subtract, divide and multiply, and oper ate

--- javascript.js
+++ javascript.js
@@ -1,1 +1,53 @@
-console.log("test");
+//Pseudo code
+
+
+let num1 = 0;
+let num2 = 0;
+let operand = "";
+
+function add (num1, num2) {
+
+    let total = num1+num2;
+
+    return (total)
+}
+
+function subtract (num1, num2) {
+    let total = num1-num2;
+
+    return (total)
+}
+
+function multiply (num1, num2) {
+    let total = num1 * num2;
+
+    return (total)
+}
+
+function divide (num1, num2) {
+    let total = num1 / num2;
+
+    return (total);
+}
+
+function operate (num1, operand, num2) {
+    if (operand === "+") {
+        return (add(num1, num2));
+    }
+    else if (operand === "-") {
+        return(subtract(num1, num2));
+    }
+    else if (operand === "/") {
+        return(divide(num1, num2));
+    }
+    else if (operand === "*") {
+        return (divide(num1, num2));
+    }
+
+    else {
+        return ("ERROR!!");
+    }
+}
+
+console.log(operate (15, "/", 5));
+

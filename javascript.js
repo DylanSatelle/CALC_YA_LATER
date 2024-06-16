38bf3abb1db0e48ab352692a42289a8b18fa83d0
Dylan Satelle <dylanjsatelle@gmail.com>
2024-06-16 11:15:38 +0100

    added query selector and additional css

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@
 let num1 = 0;
 let num2 = 0;
 let operand = "";
+let displayValue = "";
 
 function add (num1, num2) {
 
@@ -51,3 +52,20 @@ function operate (num1, operand, num2) {
 
 console.log(operate (15, "/", 5));
 
+//add event listeners to all buttons
+
+const test = document.querySelectorAll("button");
+for (let i = 0; i < test.length; i++) {
+    test[i].addEventListener("click", function(e) {updateDisplay(this.value);});
+}
+
+//function updateDisplay (clickedNumber) {
+    //const mainDisplay = document.getElementsByClassName("calculator-display");
+  //  mainDisplay.innerHTML = "33";
+//}
+
+function updateDisplay (clickedValue) {
+const mainDisplay = document.getElementById("calc-display");
+mainDisplay.innerHTML = clickedValue;
+
+}

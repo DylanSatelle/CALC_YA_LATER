6a15a381ac1280f885c2891b5e3b823262d2339a
Dylan Satelle <dylanjsatelle@gmail.com>
2024-06-16 11:54:29 +0100

    turned bbtnassign into func

--- javascript.js
+++ javascript.js
@@ -5,6 +5,9 @@ let num1 = 0;
 let num2 = 0;
 let operand = "";
 let displayValue = "";
+let runningTotal = "";
+
+btnEventListnerAssign();
 
 function add (num1, num2) {
 
@@ -50,22 +53,49 @@ function operate (num1, operand, num2) {
     }
 }
 
-console.log(operate (15, "/", 5));
+//console.log(operate (15, "/", 5));
 
-//add event listeners to all buttons
+//reset all values to start fresh
+function reset () {
+    runningTotal = "";
+    updateDisplay("");
+    num1 = 0;
+    num2 = 0;
+    operand = "";
+    displayValue = "";
+}
 
-const test = document.querySelectorAll("button");
+function btnEventListnerAssign() {
+//add event listeners to all buttons that are numbers or operators..
+const test = document.querySelectorAll(".btnnumber");
 for (let i = 0; i < test.length; i++) {
     test[i].addEventListener("click", function(e) {updateDisplay(this.value);});
 }
-
-//function updateDisplay (clickedNumber) {
-    //const mainDisplay = document.getElementsByClassName("calculator-display");
-  //  mainDisplay.innerHTML = "33";
-//}
-
-function updateDisplay (clickedValue) {
-const mainDisplay = document.getElementById("calc-display");
-mainDisplay.innerHTML = clickedValue;
-
 }
+
+
+//function update display 
+function updateDisplay (clickedValue) {
+displayValue = displayValue + "" + clickedValue;
+const mainDisplay = document.getElementById("calc-display");
+mainDisplay.innerHTML = displayValue;
+console.log("YOUR RUNNING TOTAL IS : " + displayValue);
+}
+
+function numLogic () {
+    
+}
+
+
+
+//pseudo for logic 
+/*
+
+if operand is empty 
+assign running total to num 1
+if operand isnt empty
+assign running total to num  2
+
+
+*/
+
